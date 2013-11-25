f23a999dad656890b2eb90266d565aa721d0699a
Jeremy Page <j.n.page@gmail.com>
2013-11-25 06:32:29 +0000

    Initial commit

---
+++ images/history_quiz/private/data.js
@@ -0,0 +1,144 @@
+questions=10
+
+finish='You have finished.'
+intro="Look at the pictures, work out when they were taken, and then press the correct date button."
+
+P1="Hotpot5.jpg' WIDTH='446' HEIGHT='294'"
+Q1="When was this school photo taken?"
+A1A=1900
+A1B=1925
+A1C=1950
+A1D=1975
+
+R1A="Look at the children's clothes, especially the boys' sailor suits. Now try again."
+R1B="Well done this school photo was taken in about 1925."
+R1C="Bad luck. This photo wasn't taken in 1950."
+R1D="Look at the teacher's clothes. Now try again."
+
+correct1=2
+
+P2="1895.jpg' WIDTH='386' HEIGHT='317'"
+Q2="When was this family photo taken?"
+A2A=1895
+A2B=1915
+A2C=1940
+A2D=1965
+
+R2A="Yes. Well done!"
+R2B="The youngest child is a boy! Look at his clothes, now try again."
+R2C="Look at the children's clothes. They might give you a clue. Now try again."
+R2D="Look at the lady's clothes. They might give you a clue. Now try again."
+
+correct2=1
+
+P3="1920.jpg' WIDTH='291' HEIGHT='348'"
+Q3="This is a picture of a young boy, but when was it taken?"
+A3A=1980
+A3B=1950
+A3C=1920
+A3D=1890
+
+R3A="No bad luck.Look at what the little boy is wearing. Now try again."
+R3B="Bad luck, the photo was taken much earlier than this."
+R3C="Yes well done. This little boy was born in 1919."
+R3D="No bad luck, that's not the right A. Try again."
+
+correct3=3
+
+P4="concorde.jpg' WIDTH='382' HEIGHT='257'"
+Q4="This is a photo of Concorde. When was it taken?"
+A4A=1945
+A4B=1955
+A4C=1965
+A4D=1975
+
+R4A="Concorde was not built as early as this."
+R4B="No, Concorde was built later than this."
+R4C="Find out when Concorde was built."
+R4D="Yes, well done. Concorde made its first commercial flight in the 1970s."
+
+correct4=4
+
+P5="1930.jpg' WIDTH='401' HEIGHT='220'"
+Q5="These ladies are on holiday, but when was the picture taken?"
+A5A=1910
+A5B=1930
+A5C=1950
+A5D=1970
+
+R5A="Look at the length of the ladies' skirts and try again."
+R5B="Yes, well done. These ladies were very fashionably dressed for the time."
+R5C="Bad luck. Look carefully at the clothes the ladies are wearing and try again."
+R5D="Look carefully at the fashions and try again."
+
+correct5=2
+
+P6="1908.jpg' WIDTH='206' HEIGHT='373'"
+Q6="This girl was a bridesmaid, but what was the year?"
+A6A=1908
+A6B=1938
+A6C=1968
+A6D=1998
+
+R6A="Yes, well done. This girl was bridesmaid to her eldest sister."
+R6B="Look carefully at what the girl is wearing on her head then try again."
+R6C="Bad luck, this photo was taken much earlier than this."
+R6D="Look really carefully at the photo. Think about the fashions then try again."
+
+correct6=1
+
+P7="Hotpot12.jpg' WIDTH='266' HEIGHT='353'"
+Q7="This man is in Canada. He is going to fight in France."
+A7A=1895
+A7B=1915
+A7C=1935
+A7D=1955
+
+R7A="No this is not the correct date. Think about when British soldiers were fighting in France, and then try again."
+R7B="Yes well done. This soldier was injured at the Battle of the Somme in 1916."
+R7C="No, this is not the right date.Try again."
+R7D="No, there were no British soldiers fighting in France in this year.Try again."
+
+correct7=2
+
+P8="1940a.jpg' WIDTH='113' HEIGHT='380'"
+Q8="This is a wedding photo, when was it taken?"
+A8A=1920
+A8B=1940
+A8C=1960
+A8D=1980
+
+R8A="Look carefully at the clothesbride the bridesmaid is wearing. Then try again."
+R8B="Yes well done. This is a wartime wedding. The bride and bridesmaid are cousins."
+R8C="No, bad luck, this photo was not taken in 1960."
+R8D="Look carefully at what the lady is wearing. Have another go."
+
+correct8=2
+
+P9="Hotpot3.jpg' WIDTH='380' HEIGHT='293'"
+Q9="When was this taken?"
+A9A=1935
+A9B=1955
+A9C=1975
+A9D=1995
+
+R9A="Look at the clues, and then try again."
+R9B="You need to look carefully at the background. Now try again."
+R9C="Yes, well done. This photo was taken in 1975."
+R9D="Bad luck this photo was taken earlier than 1995."
+
+correct9=3
+
+P10="1938.jpg' WIDTH='455' HEIGHT='284'"
+Q10="When was this school photo taken?"
+A10A=1938
+A10B=1928
+A10C=1918
+A10D=1908
+
+R10A="That's right. This was how you dressed for school in 1938."
+R10B="You are very close, but these students were much younger in 1928."
+R10C="The photo was taken later than 1918."
+R10D="The parents of these students were at school in 1908."
+
+correct10=1
